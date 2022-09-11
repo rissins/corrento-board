7b4dfa848a11c8549b039486cb4b9728acd0b2eb
rissins <tnalsqqq@naver.com>
2022-09-11 22:05:20 +0900

    style: 미사용 import 삭제

--- src/main/java/com/rissins/board/repository/AttachmentRepository.java
+++ src/main/java/com/rissins/board/repository/AttachmentRepository.java
@@ -1,7 +1,6 @@
 package com.rissins.board.repository;
 
 import com.rissins.board.domain.Attachment;
-import com.rissins.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
